feb7ca9fcbb18d221df882b056b454f87b539683
garlic-brewlabs <garlic@brewlabs.info>
2025-01-03 11:43:51 +1000

    Move toaster out of main

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import { ProviderAppKit } from "@/components/provider-appkit";
 import { ProviderMoralis } from "@/components/provider-moralis";
 
-import App from "./App";
-import "./index.css";
+import { Toaster } from "@/components/ui/toaster";
+
+import App from "@/App";
+import "@/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ProviderAppKit>
       <ProviderMoralis>
         <App />
+        <Toaster />
       </ProviderMoralis>
     </ProviderAppKit>
   </StrictMode>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { Toaster } from "@/components/ui/toaster";
+
 import { Navigation } from "@/components/navigation";
 import { CreatorSearch } from "@/components/creator-search";
 
@@ -8,11 +8,10 @@ export const Route = createRootRoute({
     <div className="min-h-[calc(100svh-2rem)] bg-gradient-to-b from-sky-300 to-stone-400 border-t border-stone-100 m-4 rounded-2xl shadow-sm relative before:fixed before:inset-0 before:bg-noise before:opacity-[0.015] before:pointer-events-none before:-z-10 after:fixed after:inset-0 after:bg-scanline after:pointer-events-none after:animate-scanline">
       <Navigation />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-7xl mx-auto px-0 sm:px-6 lg:px-8 sm:py-8 pt-6">
         <Outlet />
       </div>
 
-      <Toaster />
       <CreatorSearch />
     </div>
   ),
